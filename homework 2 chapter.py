9341d3b43124e3e31279d20535b050bcf03d0ba1
DanielLukin <142903637+DanielLukin@users.noreply.github.com>
2023-09-19 13:24:18 +0300

    Add files via upload
    Chapter 2

---
+++ homework 2 chapter.py
@@ -0,0 +1,52 @@
+# Module 2
+
+# number 1
+'''
+user = input('Write your name: ')
+print('Hello ' + user)
+'''
+
+#number 2
+'''
+import math
+radius = float(input('Write the radius of the circle: '))
+area = radius ** 2 * math.pi
+round_area = round(area, 2)
+print('The are of your circle is: ', round_area)
+'''
+
+#number 3
+'''
+length = float(input('Write the length of rectangle: '))
+width = float(input('Write the width of rectangle: '))
+area = width * length
+perimeter = (width + length) * 2
+print('The area of your rectangle is ', area, 'And the perimeter of your rectangle is: ', perimeter)
+'''
+
+#number 4
+'''
+number_1 = int(input('Write your first number: '))
+number_2 = int(input('Write your second number: '))
+number_3 = int(input('Write your third number: '))
+sum_numbers = number_1 + number_2 + number_3
+product_numbers = number_1 * number_2 * number_3
+average_numbers = (number_1 + number_2 + number_3) / 3
+print('The sum of your numbers is ', sum_numbers, 'The product of your numbers is ', product_numbers, 'The average of your numbers is ', average_numbers)
+'''
+
+#number 5
+'''
+number1 = float(input('Write a mass in a talents: '))
+number2 = float(input('Write a mass in a pounds: '))
+number3 = float(input('Write a mass in a lots: '))
+a = (number1 * 20 * 32 * 13.3) + (number2 * 32 * 13.3) + (number3 * 13.3)
+b = a / 1000
+c = a % 1000
+print('The weight in modern units: ', b, 'kilograms ', c, 'grams')
+'''
+
+
+
+
+
